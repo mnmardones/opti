6d5ecc6d255a71356fef92e1dbe3d83c8917e0fb
mnmardones <mnmardones@uc.cl>
2018-10-14 21:49:46 -0300

    nueva version

--- modelo.py
+++ modelo.py
@@ -27,11 +27,11 @@ h = modelo.addVars(dias, trabajador, name="H", lb=0, ub=1)
 modelo.setObjective(T, GRB.MINIMIZE)
 modelo.update()
 #Asignacion de trabajo y dias libres
-modelo.addConstrs((1 == (w[t, i] + z[t, i] + l[t, i] +
+modelo.addConstrs(((1 == w[t, i] + z[t, i] + l[t, i] +
                   quicksum((x[t, i, c] for c in cuadrilla)) if t <= T else
-                  0 == (w[t, i] + z[t, i] + l[t, i] +
+                  0 == w[t, i] + z[t, i] + l[t, i] +
                   quicksum((x[t, i, c] for c in cuadrilla))
-                     for t in dias for i in trabajador))
+                     for t in dias for i in trabajador)))
 
 #A lo más 3 cuadrillas por supervisor
 modelo.addConstrs((quicksum((y[t, i, c] for c in cuadrilla)) <= 3 * w[t, i] for t in dias for i in  trabajador))
@@ -69,3 +69,7 @@ for i in trabajadpr))if t <= T for t in dias))) >= requerido)
 
 #Mejora de habilidad por día trabajado
 modelo.addConstrs((h[t, i] == h[t - 1,i ] * (1 + gamma * quicksum((x[t - 1, i, c] for c in cuadrilla))) if 2 <= t <= T for i in trabajador for t in dias))
+
+modelo.update()
+modelo.optimize()
+modelo.printAttr("X")
